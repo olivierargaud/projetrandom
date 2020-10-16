1cf52904f0c21151381fcbccc44bf8c8944f6635
olivierargaud <olivier.argaud@ipilyon.net>
2020-10-16 10:15:11 +0200

    HTTP Methods

--- randomDice.py
+++ randomDice.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for , request
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -7,16 +7,10 @@ app = Flask(__name__)
 def index():
     return 'index'
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'login'
-
-@app.route('/user/<username>')
-def profile(username):
-    return '{}\'s profile'.format(escape(username))
-
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/salut'))
-    print(url_for('profile', username='John Doe'))
+    if request.method == 'POST':
+        return 'do_the_login() POST'
+    else:
+        return 'show_the_login_form() GET'
