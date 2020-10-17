ec639c91bd9b48384aa38e092ca495a0c0a74afc
olivierargaud <olivier.argaud@ipilyon.net>
2020-10-17 11:27:32 +0200

    premier test random

--- randomDice.py
+++ randomDice.py
@@ -1,6 +1,19 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, redirect
+import random
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return render_template('home.html')
+
+@app.route('/',methods=['POST','GET'])
+def index():
+    if request.method == 'POST':
+        valeur_max = int(request.form['valeurMax']) 
+        valeur_a_afficher =random.randint(1, valeur_max)
+        return render_template('home.html', valeur_max=valeur_max,valeur_a_afficher = valeur_a_afficher)
+       
+    else:
+        return redirect('/')
+
